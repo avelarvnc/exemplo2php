a3bd96ee65d4d730ba33a2af4e1e22d5ebbb3b71
Clovis Vinicius Avelar <avelarvnc@gmail.com>
2023-10-02 09:10:02 -0300

    update

---
+++ if.php
@@ -0,0 +1,51 @@
+<?php
+
+    $x = 10;
+
+    if ($x == 10)
+    {
+        echo "É DEZ!";
+    }
+    else
+    {
+        echo "NÃO É DEZ. :(";
+    }
+
+    $j = 5;
+
+    if ($j > 5)
+    {
+        echo "É maior do que 5";
+    }
+    else if ($j == 5)
+    {
+        echo "É 5.";
+    }
+    else
+    {
+        echo "É menor do que 5";
+    }
+
+    $nome = "Rafael";
+    if ($nome == "Adriana")
+    {
+        echo "É a Adriana, não o Matheus";
+    }
+    else if ($nome == "João")
+    {
+        echo "É o João, não o Matheus";
+    }
+    else if ($nome == "Darlon")
+    {
+        echo "É o Darlon, não o Matheus";
+    }
+    else if ($nome == "Rafael")
+    {
+        echo "É o Rafael, não o Matheus";
+    }
+    else
+    {
+        echo "É o Matheus";
+    }
+
+?>
---
+++ nomes.php
@@ -0,0 +1,26 @@
+<?php
+
+    $nome = $_GET["pessoa"];
+
+    if ($nome == "Adriana")
+    {
+        echo "É a Adriana, não o Matheus";
+    }
+    else if ($nome == "João")
+    {
+        echo "É o João, não o Matheus";
+    }
+    else if ($nome == "Darlon")
+    {
+        echo "É o Darlon, não o Matheus";
+    }
+    else if ($nome == "Rafael")
+    {
+        echo "É o Rafael, não o Matheus";
+    }
+    else
+    {
+        echo "É o Matheus";
+    }
+
+?>
